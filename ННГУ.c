11ef12f18ce70ae6b5c7aa8cc8ba26ce218d7c9e
Tatiana567 <90023891+Tatiana567@users.noreply.github.com>
2021-09-17 09:17:42 +0300

    Add files via upload

---
+++ ННГУ.c
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+int main()
+{
+    int a = 0;
+    char c = 'v';
+    long long int d = 465466435;
+    short b = 3;
+    unsigned short p = 9;
+    unsigned int k = 90;
+    long i = 8989;
+    float f = 8.909;
+    double g = 88.9000;
+    unsigned char h = 'g';
+    long double l = 98.8998;
+
+    printf("c=%c\n", c);
+    printf("d=%d\n", d);
+    printf("a=%d\n", a);
+    printf("i=%d\n", i);
+    printf("f=%f\n", f);
+    printf("g=%f\n", g);
+    printf("h=%c\n", h);
+    printf("p=%d\n", p);
+    printf("k=%d\n", k);
+    printf("l=%f\n", l);
+    return 0;
+
+}
+
